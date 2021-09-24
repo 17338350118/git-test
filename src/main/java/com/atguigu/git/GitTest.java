d753acf2edd9fe6355946f3d588ca77ee603ae2d
Dream <51253818+17338350118@users.noreply.github.com>
2021-09-25 01:21:55 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hot");
         System.out.println("master1");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
